9c162fd6c126e7ddec0c823ef57e2ce3794d7b61
SharonF11 <sharon@fungalaxy.ie>
2022-07-06 20:42:56 +0100

    AddingGoogleMap

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes),
+  GoogleMapsModule],
   exports: [RouterModule]
 })
 export class Tab1PageRoutingModule {}
